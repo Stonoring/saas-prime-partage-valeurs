28f5d699dd2c53f561eeafe007a24881b5afb6b7
Stonoring <marius.brunet@finopia.fr>
2024-12-03 15:58:45 +0100

    Disable ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Désactive ESLint pendant le build
+  },
   /* config options here */
 };
 
